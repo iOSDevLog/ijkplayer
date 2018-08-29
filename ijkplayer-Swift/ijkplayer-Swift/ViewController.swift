be27013ed19a7c5de6e904c8c4d51d52ce8a30cf
iosdevlog <iosdevlog@iosdevlog.com>
2018-08-30 00:18:24 +0800

    refactor

--- ijkplayer-Swift/ijkplayer-Swift/ViewController.swift
+++ ijkplayer-Swift/ijkplayer-Swift/ViewController.swift
@@ -15,13 +15,9 @@ class ViewController: UIViewController {
                 "rtmp://v1.one-tv.com:1935/live/mpegts.stream"]
     
     @IBOutlet weak var videoView: UIView!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
+    fileprivate func play(_ url: URL?) {
         let options = IJKFFOptions.byDefault()
         
-        //视频源地址
-        let url = URL(string: urls[1])
         
         //初始化播放器，播放在线视频或直播（RTMP）
         let player = IJKFFMoviePlayerController(contentURL: url!, with: options)
@@ -35,6 +31,14 @@ class ViewController: UIViewController {
         self.videoView.autoresizesSubviews = true
         self.videoView.addSubview((player?.view)!)
         self.player = player
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //视频源地址
+        let url = URL(string: urls[1])
+        
+        play(url)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -53,20 +57,7 @@ class ViewController: UIViewController {
         self.player.shutdown()
         
         let url = URL(string: urls[0])
-        
-        let options = IJKFFOptions.byDefault()
-        //初始化播放器，播放在线视频或直播（RTMP）
-        let player = IJKFFMoviePlayerController(contentURL: url!, with: options)
-        //播放页面视图宽高自适应
-        player?.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        
-        player?.view.frame = self.videoView.bounds
-        player?.scalingMode = .aspectFit //缩放模式
-        player?.shouldAutoplay = true //开启自动播放
-        
-        self.videoView.autoresizesSubviews = true
-        self.videoView.addSubview((player?.view)!)
-        self.player = player
+        play(url)
         self.player.prepareToPlay()
     }
     
@@ -74,20 +65,7 @@ class ViewController: UIViewController {
         self.player.shutdown()
         
         let url = URL(string: urls[1])
-        
-        let options = IJKFFOptions.byDefault()
-        //初始化播放器，播放在线视频或直播（RTMP）
-        let player = IJKFFMoviePlayerController(contentURL: url!, with: options)
-        //播放页面视图宽高自适应
-        player?.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        
-        player?.view.frame = self.videoView.bounds
-        player?.scalingMode = .aspectFit //缩放模式
-        player?.shouldAutoplay = true //开启自动播放
-        
-        self.videoView.autoresizesSubviews = true
-        self.videoView.addSubview((player?.view)!)
-        self.player = player
+        play(url)
         self.player.prepareToPlay()
     }
 }
